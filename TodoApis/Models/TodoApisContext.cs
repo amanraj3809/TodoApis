953e3d2bfd1fc3b1159f24a8d41ca8324da1fa60
amanraj3809 <aman.raj@hashedin.com>
2017-01-06 12:57:42 +0530

    Adding models

---
+++ TodoApis/Models/TodoApisContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TodoApis.Models
+{
+    public class TodoApisContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TodoApisContext() : base("name=TodoApisContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TodoApis.Models.message> Messages { get; set; }
+    
+    }
+}
