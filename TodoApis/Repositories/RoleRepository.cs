ce30455cee709a932ffd0a31f7e795eb6fbe969b
amanraj3809 <aman.raj@hashedin.com>
2017-01-06 13:05:12 +0530

    Adding RoleRepository

---
+++ TodoApis/Repositories/RoleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TodoApis.IRepositories;
+using TodoApis.Models;
+
+namespace TodoApis.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        IderaCWFRepositoryEntities2 CWFRepository;
+
+        public RoleRepository(IderaCWFRepositoryEntities2 cwfRepository)
+        {
+            this.CWFRepository = cwfRepository;
+        }
+
+        public List<Models.Role> GetAllRoles()
+        {
+            return CWFRepository.Roles.ToList();
+        }
+    }
+}
---
+++ TodoApis/Services/RoleService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TodoApis.IRepositories;
+using TodoApis.IServices;
+using TodoApis.Models;
+
+namespace TodoApis.Services
+{
+    public class RoleService : IRoleService
+    {
+        IRoleRepository RoleRepository;
+
+        public RoleService(IRoleRepository roleRepository)
+        {
+            this.RoleRepository = roleRepository;
+        }
+
+        public List<Role> GetRoles()
+        {
+            return RoleRepository.GetAllRoles();
+        }
+    }
+}
