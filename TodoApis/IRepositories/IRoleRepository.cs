0dfd3b20c525f89e97c26ce0fcdf28c8ef945a0b
amanraj3809 <aman.raj@hashedin.com>
2017-01-06 13:04:34 +0530

    Adding INterfaces for repositories and services

---
+++ TodoApis/IRepositories/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TodoApis.Models;
+
+namespace TodoApis.IRepositories
+{
+    public interface IRoleRepository
+    {
+        List<Role> GetAllRoles();
+
+    }
+}
---
+++ TodoApis/IServices/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApis.Models;
+
+namespace TodoApis.IServices
+{
+    public interface IRoleService
+    {
+        List<Role> GetRoles();
+    }
+}
